f8ba6dcc1e842607298dd7b27ce67c534a53c971
ypldan <ydpl1998@gmail.com>
2019-03-03 18:43:50 +0300

    added model, added drawing 4 types of figures

--- lab1/figures.py
+++ lab1/figures.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from tkinter import Canvas
 
 
 class Point2D:
@@ -8,99 +9,115 @@ class Point2D:
         self.y = y
 
 
-class Color:
-
-    def __init__(self, r: int, g: int, b: int):
-        self.r = r
-        self.g = g
-        self.b = b
-
-
 class Drawable(ABC):
 
-    def __init__(self, color: Color, fill: Color):
-        self.__color = color
-        self.__fill = fill
+    def __init__(self, color: str, fill: str, width: int):
+        self.color = color
+        self.fill = fill
+        self.width = width
 
     @abstractmethod
-    def draw(self):
+    def draw(self, canvas: Canvas):
         pass
 
 
-class Figure(ABC, Drawable):
+class Movable(ABC):
 
-    def __init__(self, center: Point2D, color: Color, fill: Color):
-        Drawable.__init__(self, color, fill)
+    @abstractmethod
+    def move(self, coordinates: tuple, canvas: Canvas):
+        pass
+
+    @abstractmethod
+    def get_location(self, canvas: Canvas):
+        pass
+
+
+class Figure(Drawable, Movable):
+
+    def __init__(self, center: Point2D, color: str, fill: str, width: int):
+        Drawable.__init__(self, color, fill, width)
         self.center = center
 
-    @abstractmethod
-    def move(self, coordinates: tuple):
-        pass
+    def move(self, coordinates: tuple, canvas: Canvas):
+        self.center = Point2D(coordinates[0], coordinates[1])
 
-    @abstractmethod
-    def get_location(self):
-        pass
+    def get_location(self, canvas: Canvas):
+        return self.center
 
 
 class Line(Drawable):
 
-    def __init__(self, point1: Point2D, point2: Point2D, color: Color,
-                 fill: Color):
-        Drawable.__init__(self, color, fill)
+    def __init__(self, point1: Point2D, point2: Point2D, color: str, fill: str, width: int):
+        Drawable.__init__(self, color, fill, width)
         self.__p1 = point1
         self.__p2 = point2
 
 
 class Ray(Line):
 
-    def __init__(self, center: Point2D, point: Point2D, color: Color,
-                 fill: Color):
-        Line.__init__(self, center, point, color, fill)
+    def __init__(self, center: Point2D, point: Point2D, color: str, fill: str, width: int):
+        Line.__init__(self, center, point, color, fill, width)
 
 
 class Segment(Ray):
 
-    def __init__(self, point1: Point2D, point2: Point2D, color: Color,
-                 fill: Color):
-        Ray.__init__(self, point1, point2, color, fill)
+    def __init__(self, point1: Point2D, point2: Point2D, color: str, fill: str, width: int):
+        Ray.__init__(self, point1, point2, color, fill, width)
+
+    def draw(self, canvas: Canvas):
+        canvas.create_line(self.__p1.x, self.__p1.y,
+                           self.__p2.x, self.__p2.y,
+                           fill=self.fill, outline=self.color, width=self.width)
 
 
 class Rectangle(Figure):
 
-    def __init__(self, center: Point2D, color: Color, fill: Color,
+    def __init__(self, center: Point2D, color: str, fill: str, width: int,
                  a: int, b=None):
-        Figure.__init__(self, center, color, fill)
+        Figure.__init__(self, center, color, fill, width)
         self.a = a
         self.b = a if b is None else b
+
+    def draw(self, canvas: Canvas):
+        x1 = self.center.x - self.a // 2
+        y1 = self.center.y - self.b // 2
+        x2 = self.center.x + self.a // 2
+        y2 = self.center.y + self.b // 2
+        canvas.create_rectangle(x1, y1, x2, y2, fill=self.fill, outline=self.color, width=self.width)
 
 
 class Square(Rectangle):
 
-    def __init__(self, center: Point2D, color: Color, fill: Color,
+    def __init__(self, center: Point2D, color: str, fill: str, width: int,
                  a: int):
-        Rectangle.__init__(self, center, color, fill, a)
+        Rectangle.__init__(self, center, color, fill, width, a)
 
 
-class Ellipse(Figure):
+class Ellipse(Rectangle):
 
-    def __init__(self, center: Point2D, color: Color, fill: Color,
-                 radius1: int, radius2=None):
-        Figure.__init__(self, center, color, fill)
-        self.r1 = radius1
-        self.r2 = radius1 if radius2 is None else radius2
+    def __init__(self, center: Point2D, color: str, fill: str, width: int,
+                 a: int, b=None):
+        Rectangle.__init__(self, center, color, fill, width, a, b)
+
+    def draw(self, canvas: Canvas):
+        x1 = self.center.x - self.a // 2
+        y1 = self.center.y - self.b // 2
+        x2 = self.center.x + self.a // 2
+        y2 = self.center.y + self.b // 2
+        canvas.create_oval(x1, y1, x2, y2, fill=self.fill, outline=self.color, width=self.width)
 
 
 class Circle(Ellipse):
 
-    def __init__(self, center: Point2D, color: Color, fill: Color,
+    def __init__(self, center: Point2D, color: str, fill: str, width: int,
                  radius: int):
-        Ellipse.__init__(self, center, color, fill, radius)
+        Ellipse.__init__(self, center, color, fill, width, radius)
 
 
 class Rhombus(Figure):
 
-    def __init__(self, center: Point2D, color: Color, fill: Color,
+    def __init__(self, center: Point2D, color: str, fill: str, width: int,
                  diag_length1: int, diag_length2: int):
-        Figure.__init__(self, center, color, fill)
+        Figure.__init__(self, center, color, fill, width)
         self.d1 = diag_length1
         self.d2 = diag_length2
---
+++ lab1/main_program.py
@@ -0,0 +1,110 @@
+from tkinter import *
+from tkinter.colorchooser import askcolor
+import tkinter.font as tkfont
+from figures import *
+
+
+class MainWindow(object):
+    __MENU_FONT = None
+    __DEFAULT_COLOR = 'black'
+
+    def __init__(self):
+        self.__line_width = 1
+        self.__figure = Square(Point2D(100,100), self.__DEFAULT_COLOR, self.__DEFAULT_COLOR, self.__line_width, 70)
+        self.root = Tk()
+        self.__MENU_FONT = tkfont.Font(family="fangsong ti", size=16)
+        self.__create_menu()
+        self.c = Canvas(self.root, bg='white', width=800, height=500)
+        self.c.bind("<Button-1>", self.paint)
+        self.c.pack(fill=BOTH, expand=True)
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.mainloop()
+
+    def __create_menu(self):
+        self.menu = Menu(self.root)
+        self.figures_menu = Menu(self.menu, tearoff=0)
+        self.__create_figure_menu()
+        self.menu.add_cascade(label="Create", menu=self.figures_menu, font=self.__MENU_FONT)
+        self.__create_settings_menu()
+        self.menu.add_cascade(label='Settings', menu=self.settings_menu, font=self.__MENU_FONT)
+        self.root.config(menu=self.menu)
+
+    def __create_figure_menu(self):
+        self.figures_menu = Menu(self.menu, tearoff=0)
+        create_menu = [
+            # {'label': 'Line', 'command': lambda: print('line')},
+            # {'label': 'Ray', 'command': lambda: print('ray')},
+            {'label': 'Circle', 'command': self.__set_circle},
+            {'label': 'Ellipse', 'command': self.__set_ellipse},
+            # {'label': 'Triangle', 'command': lambda: print('triangle')},
+            {'label': 'Rectangle', 'command': self.__set_rectangle},
+            {'label': 'Square', 'command': self.__set_square}
+        ]
+        for config in create_menu:
+            config['font'] = self.__MENU_FONT
+            self.figures_menu.add_radiobutton(config)
+
+    def __create_settings_menu(self):
+        self.settings_menu = Menu(self.menu, tearoff=0)
+        settings_menu = [
+            {'label': 'Choose line color', 'command': self.__set_line_color},
+            {'label': 'Choose fill color', 'command': self.__set_fill_color},
+            {'label': 'Set line width', 'command': lambda: WidthDialog(self)},
+            {'label': 'Clear', 'command': self.__clear_canvas}
+        ]
+        for config in settings_menu:
+            config['font'] = self.__MENU_FONT
+            self.settings_menu.add_command(config)
+
+    def __set_fill_color(self):
+        self.__figure.fill = askcolor(color=self.__figure.fill)[1]
+
+    def __set_line_color(self):
+        self.__figure.color = askcolor(color=self.__figure.color)[1]
+
+    def __set_figure(self, figure: Figure):
+        self.__figure = figure
+
+    def __set_circle(self):
+        self.__set_figure(Circle(self.__figure.center,
+                                 self.__figure.color,
+                                 self.__figure.fill,
+                                 self.__line_width, 50))
+
+    def __set_square(self):
+        self.__set_figure(Square(self.__figure.center,
+                                 self.__figure.color,
+                                 self.__figure.fill,
+                                 self.__line_width, 50))
+
+    def __set_rectangle(self):
+        self.__set_figure(Rectangle(self.__figure.center,
+                                    self.__figure.color,
+                                    self.__figure.fill,
+                                    self.__line_width, 70, 50))
+
+    def __set_ellipse(self):
+        self.__set_figure(Ellipse(self.__figure.center,
+                                    self.__figure.color,
+                                    self.__figure.fill,
+                                    self.__line_width, 70, 50))
+
+    def __clear_canvas(self):
+        self.c.delete('all')
+
+    def paint(self, event):
+        self.__figure.center = Point2D(event.x, event.y)
+        self.__figure.draw(self.c)
+
+
+class WidthDialog:
+
+    def __init__(self, main: MainWindow):
+        self.top = Toplevel(main.root)
+        self.choose_size_button = Scale(self.top, from_=1, to=10, orient=HORIZONTAL)
+        self.choose_size_button.pack()
+
+
+if __name__ == '__main__':
+    MainWindow()
