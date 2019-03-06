97a641a90ca8e771957347f1364e2f2dd31c1500
ypldan <ydpl1998@gmail.com>
2019-03-06 13:29:58 +0300

    added line, fixed bugs

--- lab1/figures.py
+++ lab1/figures.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from tkinter import Canvas
 from math import copysign as copysign_f
+import numpy as np
 
 
 def copysign(x, y) -> int:
@@ -15,7 +16,8 @@ class Point2D:
 
     @staticmethod
     def mid_point(p1, p2):
-        return Point2D(abs(p1.x - p2.x) // 2, abs(p1.y - p2.y) // 2)
+        return Point2D(min(p1.x, p2.x) + int(abs(p1.x - p2.x) / 2),
+                       min(p1.y, p2.y) + int(abs(p1.y - p2.y) / 2))
 
     @staticmethod
     def distance(p1, p2):
@@ -48,20 +50,23 @@ class Figure(ABC):
         self.p2.y += dy
         self.center = new_center
         if self.drawn is not None:
-            canvas.coords(self.drawn, self.p1.x, self.p1.y, self.p2.x, self.p2.y)
+            canvas.coords(self.drawn, self.get_location()[2:])
 
     def get_location(self) -> tuple:
         return self.center.x, self.center.y, self.p1.x, self.p1.y, self.p2.x, self.p2.y
 
-    def move_points(self, p1: Point2D, p2: Point2D):
+    def move_points(self, p1: Point2D, p2: Point2D, canvas=None):
         self.p1 = p1
         self.p2 = p2
         self.center = Point2D.mid_point(p1, p2)
+        if canvas is not None and self.drawn is not None:
+            canvas.coords(self.drawn, self.get_location()[2:])
 
     def is_drawn(self) -> bool:
         return self.drawn is not None
 
     def update(self, canvas: Canvas):
+        canvas.delete(self.drawn)
         self.drawn = None
         self.draw(canvas)
 
@@ -86,13 +91,17 @@ class Square(Rectangle):
         super().__init__(color, fill, width, p1, p2)
         self.move_points(p1, p2)
 
-    def move_points(self, p1: Point2D, p2: Point2D):
-        dx, dy = Point2D.offset(self.p1, self.p2)
-        if abs(dy) > abs(dx):
-            self.p2.y = self.p1.y + copysign(dx, dy)
-        elif abs(dy) < abs(dx):
-            self.p2.x = self.p1.x + copysign(dy, dx)
+    def move_points(self, p1: Point2D, p2: Point2D, canvas=None):
+        dx, dy = Point2D.offset(p1, p2)
+        self.p1 = p1
+        self.p2 = p2
+        if abs(dy) < abs(dx):
+            self.p2.y = p1.y + copysign(dx, dy)
+        elif abs(dy) > abs(dx):
+            self.p2.x = p1.x + copysign(dy, dx)
         self.center = Point2D.mid_point(self.p1, self.p2)
+        if canvas is not None and self.drawn is not None:
+            canvas.coords(self.drawn, self.get_location()[2:])
 
 
 class Ellipse(Figure):
@@ -111,16 +120,20 @@ class Circle(Ellipse):
         super().__init__(color, fill, width, p1, p2)
         self.move_points(p1, p2)
 
-    def move_points(self, p1: Point2D, p2: Point2D):
-        dx, dy = Point2D.offset(self.p1, self.p2)
-        if abs(dy) > abs(dx):
-            self.p2.y = self.p1.y + copysign(dx, dy)
-        elif abs(dy) < abs(dx):
-            self.p2.x = self.p1.x + copysign(dy, dx)
+    def move_points(self, p1: Point2D, p2: Point2D, canvas=None):
+        dx, dy = Point2D.offset(p1, p2)
+        self.p1 = p1
+        self.p2 = p2
+        if abs(dy) < abs(dx):
+            self.p2.y = p1.y + copysign(dx, dy)
+        elif abs(dy) > abs(dx):
+            self.p2.x = p1.x + copysign(dy, dx)
         self.center = Point2D.mid_point(self.p1, self.p2)
+        if canvas is not None and self.drawn is not None:
+            canvas.coords(self.drawn, self.get_location()[2:])
 
 
-class Rhombus(Figure):
+class Triangle(Figure):
 
     def draw(self, canvas: Canvas):
         if self.drawn is not None:
@@ -128,6 +141,16 @@ class Rhombus(Figure):
         else:
             self.drawn = canvas.create_polygon(self.get_location()[2:], fill=self.fill,
                                                outline=self.color, width=self.width)
+
+    def get_location(self):
+        dx, dy = Point2D.offset(self.p1, self.p2)
+        return (self.center.x, self.center.y,
+                self.p1.x + dx // 2, self.p1.y,
+                self.p1.x, self.p2.y,
+                self.p2.x, self.p2.y)
+
+
+class Rhombus(Triangle):
 
     def get_location(self):
         dx, dy = Point2D.offset(self.p1, self.p2)
@@ -142,7 +165,48 @@ class Segment(Figure):
 
     def draw(self, canvas: Canvas):
         if self.drawn is not None:
-            canvas.coords(tuple(self.drawn) + self.get_location()[2:])
+            canvas.coords(self.drawn, self.get_location()[2:])
         else:
-            self.drawn = canvas.create_rectangle(self.get_location()[2:], fill=self.fill,
-                                                 outline=self.color, width=self.width)
+            self.drawn = canvas.create_line(self.get_location()[2:], fill=self.color, width=self.width)
+
+
+class Ray(Segment):
+
+    def __init__(self, color: str, fill: str, width: int, p1: Point2D, p2: Point2D):
+        super().__init__(color, fill, width, p1, p2)
+        self.move_points(p1, p2)
+
+    def move_points(self, p1: Point2D, p2: Point2D, canvas=None):
+        dx, dy = Point2D.offset(p1, p2)
+        tan = abs(dy / dx) if dx != 0 else np.inf
+        self.p1 = p1
+        self.p2.x = self.p1.x + 10000 if p1.x < p2.x else self.p1.x -10000
+        if p1.y < p2.y:
+            self.p2.y = p1.y + int(abs(self.p2.x) * tan)
+        elif p1.y > p2.y:
+            self.p2.y = p1.y - int(abs(self.p2.x) * tan)
+        self.center = p1
+        if canvas is not None and self.drawn is not None:
+            canvas.coords(self.drawn, self.get_location()[2:])
+
+
+class Line(Ray):
+
+    def move_points(self, p1: Point2D, p2: Point2D, canvas=None):
+        dx, dy = Point2D.offset(p1, p2)
+        tan = abs(dy / dx) if dx != 0 else np.inf
+        self.center = Point2D.mid_point(p1, p2)
+        if p1.x > p2.x:
+            self.p1.x = self.center.x + 10000
+            self.p2.x = self.center.x - 10000
+        elif p1.x < p2.x:
+            self.p1.x = self.center.x - 10000
+            self.p2.x = self.center.x + 10000
+        if p1.y < p2.y:
+            self.p2.y = self.center.y + 10000 * tan
+            self.p1.y = self.center.y - 10000 * tan
+        elif p1.y > p2.y:
+            self.p2.y = self.center.y - 10000 * tan
+            self.p1.y = self.center.y + 10000 * tan
+        if canvas is not None and self.drawn is not None:
+            canvas.coords(self.drawn, self.get_location()[2:])
--- lab1/main_program.py
+++ lab1/main_program.py
@@ -2,6 +2,12 @@ from tkinter import *
 from tkinter.colorchooser import askcolor
 import tkinter.font as tkfont
 from figures import *
+from enum import Enum
+
+
+class Mode(Enum):
+    DRAW = 1
+    MOVE = 2
 
 
 class MainWindow(object):
@@ -10,13 +16,15 @@ class MainWindow(object):
 
     def __init__(self):
         self.__current = None
-        self.__line_width = 1
-        self.__figure = Square(self.__DEFAULT_COLOR, self.__DEFAULT_COLOR, 2, Point2D(20, 20), Point2D(100, 100))
+        self.__mode = Mode.DRAW
+        self.__line_width = 5
+        self.__figure = Square(self.__DEFAULT_COLOR, self.__DEFAULT_COLOR, 5, Point2D(20, 20), Point2D(100, 100))
         self.root = Tk()
         self.__MENU_FONT = tkfont.Font(family="fangsong ti", size=16)
         self.__create_menu()
         self.c = Canvas(self.root, bg='white', width=800, height=500)
-        self.c.bind("<Button-1>", self.paint)
+        self.c.bind('<Button-1>', self.press_button)
+        self.c.bind('<ButtonRelease-1>', self.release_button)
         self.c.pack(fill=BOTH, expand=True)
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
@@ -34,11 +42,12 @@ class MainWindow(object):
     def __create_figure_menu(self):
         self.figures_menu = Menu(self.menu, tearoff=0)
         create_menu = [
-            # {'label': 'Line', 'command': lambda: print('line')},
-            # {'label': 'Ray', 'command': lambda: print('ray')},
+            {'label': 'Segment', 'command': self.__set_segment},
+            {'label': 'Ray', 'command': self.__set_ray},
+            {'label': 'Line', 'command': self.__set_line},
             {'label': 'Circle', 'command': self.__set_circle},
             {'label': 'Ellipse', 'command': self.__set_ellipse},
-            # {'label': 'Triangle', 'command': lambda: print('triangle')},
+            {'label': 'Triangle', 'command': self.__set_triangle},
             {'label': 'Rectangle', 'command': self.__set_rectangle},
             {'label': 'Square', 'command': self.__set_square},
             {'label': 'Rhombus', 'command': self.__set_rhombus}
@@ -52,7 +61,6 @@ class MainWindow(object):
         settings_menu = [
             {'label': 'Choose line color', 'command': self.__set_line_color},
             {'label': 'Choose fill color', 'command': self.__set_fill_color},
-            {'label': 'Set line width', 'command': lambda: WidthDialog(self)},
             {'label': 'Clear', 'command': self.__clear_canvas}
         ]
         for config in settings_menu:
@@ -71,6 +79,7 @@ class MainWindow(object):
 
     def __set_figure(self, figure: Figure):
         self.__figure = figure
+        self.__mode = Mode.DRAW
 
     def __set_circle(self):
         self.__set_figure(Circle(self.__figure.color,
@@ -107,21 +116,55 @@ class MainWindow(object):
                                   Point2D(20, 20),
                                   Point2D(100, 150)))
 
+    def __set_triangle(self):
+        self.__set_figure(Triangle(self.__figure.color,
+                                   self.__figure.fill,
+                                   self.__line_width,
+                                   Point2D(20, 20),
+                                   Point2D(100, 150)))
+
+    def __set_segment(self):
+        self.__set_figure(Segment(self.__figure.color,
+                                  self.__figure.fill,
+                                  self.__line_width,
+                                  Point2D(20, 20),
+                                  Point2D(100, 150)))
+
+    def __set_ray(self):
+        self.__set_figure(Ray(self.__figure.color,
+                              self.__figure.fill,
+                              self.__line_width,
+                              Point2D(20, 20),
+                              Point2D(100, 150)))
+
+    def __set_line(self):
+        self.__set_figure(Line(self.__figure.color,
+                               self.__figure.fill,
+                               self.__line_width,
+                               Point2D(20, 20),
+                               Point2D(100, 150)))
+
     def __clear_canvas(self):
         self.c.delete('all')
         self.__figure.drawn = None
 
-    def paint(self, event):
-        self.__figure.move(Point2D(event.x, event.y), self.c)
-        self.__figure.draw(self.c)
+    def release_button(self, event):
+        if self.__mode == Mode.DRAW:
+            self.__figure.move_points(self.__current, Point2D(event.x, event.y))
+            if not self.__figure.is_drawn():
+                self.__figure.draw(self.c)
+            self.__mode = Mode.MOVE
+            self.__current = None
 
+    def press_button(self, event):
+        if self.__mode == Mode.DRAW:
+            self.__current = Point2D(event.x, event.y)
+        elif self.__mode == Mode.MOVE:
+            self.__figure.move(Point2D(event.x, event.y), self.c)
 
-class WidthDialog:
-
-    def __init__(self, main: MainWindow):
-        self.top = Toplevel(main.root)
-        self.choose_size_button = Scale(self.top, from_=1, to=10, orient=HORIZONTAL)
-        self.choose_size_button.pack()
+    # def paint(self, event):
+    #     self.__figure.move(Point2D(event.x, event.y), self.c)
+    #     self.__figure.draw(self.c)
 
 
 if __name__ == '__main__':
